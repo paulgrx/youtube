bd93b53354bea3760ac37b240581d695354d96a5
Paul <koncewiczpaul@gmail.com>
2023-12-04 22:30:32 +0100

    Adding navbars/buttons

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -20,5 +20,21 @@
                     </form>
                 </div>
         </header>
+        <div class="content">
+<!--            <button class="dynamic-button" onclick="alert('Button 1 clicked')">Button 1</button>
+            <button class="dynamic-button" onclick="alert('Button 2 clicked')">Button 2</button>-->
+            <button type="button" class="btn btn-outline-primary">Primary</button>
+            <button type="button" class="btn btn-outline-secondary">Secondary</button>
+            <button type="button" class="btn btn-outline-success">Success</button>
+            <button type="button" class="btn btn-outline-danger">Danger</button>
+            <button type="button" class="btn btn-outline-warning">Warning</button>
+            <button type="button" class="btn btn-outline-info">Info</button>
+            <button type="button" class="btn btn-outline-light">Light</button>
+            <button type="button" class="btn btn-outline-dark">Dark</button>
+
+
+        </div>
+
+
     </body>
 </html>
