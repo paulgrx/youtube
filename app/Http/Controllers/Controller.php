bf05154d088331a658eba04b61b853421bb69096
Paul <koncewiczpaul@gmail.com>
2024-01-30 20:28:24 +0100

    Return data from API

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -4,8 +4,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
+use Illuminate\Http\Client\Request;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\DB;
+use GuzzleHttp\Client as GuzzleClient;
 
 class Controller extends BaseController
 {
@@ -13,8 +15,35 @@ class Controller extends BaseController
 
     public function index()
     {
+//        $headers = [
+//            'Content-Type' => 'application/json'
+//        ];
+//
+//        $client = new GuzzleClient([
+//            'headers' => $headers
+//        ]);
+//
+//
+//        $r = $client->request('GET', 'https://youtube.googleapis.com/youtube/v3/videos?id='.$req->url.'&part=snippet,contentDetails,statistics,status&key=AIzaSyBZtu12_Tw0uGlktqYeo3RF8mI0Ztus1Bo');
+//        $response = $r->getBody()->getContents();
+//
+//        echo '<pre>';
+//        print_r($response);
+//        echo '<pre>';
+//
+//        die;
+        
         $data = DB::table('videos')->get()->all();
 
         return view('index')->with(['data' => $data]);
+    }
+
+    public function add() {
+
+    }
+
+    public function createVideo(Request $req)
+    {
+
     }
 }
